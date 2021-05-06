da6d1f08e3f789af967d2eab7214ae7de28ea52a
Richo Ha <richard.dqh@gmail.com>
2021-05-06 18:43:34 +0800

    Edited license function

---
+++ Develop/fgfdgReadMe.md
@@ -0,0 +1,38 @@
+
+  # fgfdg
+  undefined
+  undefined
+  undefined
+
+
+    ## TABLE OF CONTENTS
+    -   [DESCRIPTION](#description)
+    -   [INSTALLATION](#installation)
+    -   [USAGE](#usage)
+    -   [CONTRIBUTING](#contributing)
+    -   [TEST](#test)
+    -   [LICENSE](#license)
+    -   [QUESTIONS](#question)
+
+
+    ##  DESCRIPTION
+    daghf
+
+    ##  INSTALLATION
+    gad
+
+    ##  USAGE
+    fghf
+
+    ## CONTRIBUTING
+    undefined
+
+    ## TEST
+    fadh
+
+    ## LICENSE
+    undefined
+
+    ## QUESTIONS
+    To find how the application was made please see the link to my GitHub account 
+    undefined
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,11 +1,17 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(userInput) {
+  var licenseBadge;
+  var licenselink;
+  var takeMetoLicense;
+
+  // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(userLicence) { 
-  if (userlicence=="MIT"){
+function renderLicenseBadge(licenseBadge) { 
+  if (licenseBadge=="MIT"){
     licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-  } else if (userlicence=="GNU GPL"){
+  } else if (licenseBadge=="GNU GPL"){
     licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0"
-  } else if (userlicence=="Apache"){
+  } else if (licenseBadge=="Apache"){
     licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
   } else {
     licenseBadge = ""
@@ -16,12 +22,12 @@ function renderLicenseBadge(userLicence) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(userLicence) { 
-  if (userlicence=="MIT"){
+function renderLicenseLink(licenselink) { 
+  if (licenselink=="MIT"){
     licenselink= 'https://opensource.org/licenses/MIT'
-  } else if (userlicence=="GNU GPL"){
+  } else if (licenselink=="GNU GPL"){
     licenselink= 'https://www.gnu.org/licenses/gpl-3.0'
-  } else if (userlicence=="Apache"){
+  } else if (licenselink=="Apache"){
     licenselink= 'http://www.apache.org/licenses/'
   } else {
     licenselink = ""
@@ -32,7 +38,7 @@ function renderLicenseLink(userLicence) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(userLicence) { 
+function renderLicenseSection(LicenseSection) { 
   if (LicenseSection=="MIT"){
     LicenseSection= `[Go to licence](#userlicence)`
   } else if (LicenseSection=="GNU GPL"){
@@ -46,12 +52,6 @@ function renderLicenseSection(userLicence) {
   return LicenseSection;
 }
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(userInput) {
-  var licenseBadge;
-  var licenselink;
-  var takeMetoLicense;
-
   renderLicenseBadge(userInput.license);
   renderLicenseLink(userInput.license);
   renderLicenseSection(userInput.license);
@@ -62,7 +62,7 @@ function generateMarkdown(userInput) {
   ${takeMetoLicense}
   ${licenselink}
 
-  
+
     ## TABLE OF CONTENTS
     -   [DESCRIPTION](#description)
     -   [INSTALLATION](#installation)
